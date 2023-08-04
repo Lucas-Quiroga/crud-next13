3dfd4d0cba177bdc0bb3d7dca59d8f4bdff3e1f1
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-08-04 14:56:17 -0300

    localstorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,14 +1,21 @@
 import { useState, useEffect } from "react";
 
-export function useLocalStorage(key, initialValue) {
-  const [state, setState] = useState(() => {
-    const storedValue = localStorage.getItem(key);
-    return storedValue ? JSON.parse(storedValue) : initialValue;
-  });
+export function useLocalStorage(key, initialState) {
+  const [state, setState] = useState(initialState);
 
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(state));
-  }, [key, state]);
+    const item = localStorage.getItem(key);
+    const tasks = JSON.parse(item);
+    if (tasks) {
+      setState(tasks);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (state.length > 0) {
+      localStorage.setItem(key, JSON.stringify(state));
+    }
+  }, [state]);
 
   return [state, setState];
 }
