742bb7a45d47a1f11c863ae584d5db4b673fc569
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-08-04 15:43:50 -0300

    center

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Layout = ({ children }) => {
   return (
     <div className="text-white h-[calc(100vh-1.5rem)]">
-      <main className="h-5/6 py-10">{children}</main>
+      <main className="flex justify-center h-5/6 py-10">{children}</main>
     </div>
   );
 };
