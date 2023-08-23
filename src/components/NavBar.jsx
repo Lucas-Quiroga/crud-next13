e624a8590a4ea38784de121fdfe70248f8c8c282
Lucas-Quiroga <quiroga.lucasoffice@gmail.com>
2023-08-23 17:41:10 -0300

    finalizando el deploy

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -26,7 +26,7 @@ export const NavBar = () => {
               height={30}
               alt="TodoIcon"
             />
-            HOME
+            <span className="text-white">HOME</span>
           </Link>
           <ul className="flex justify-between gap-x-2 text-white">
             {session ? (
